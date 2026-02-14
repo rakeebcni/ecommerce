a76ec6e05d9fcb95e79a2a27d5ac8fd109bff76d
rakeeb molla <rakeeb@rakeebs-MacBook-Pro.local>
2026-02-14 10:27:32 +0600

    store & Cart Functionality

---
+++ carts/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from .import views
+
+
+urlpatterns = [
+
+    path('', views.cart, name= 'cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name= 'add_cart'),
+    path('remove_cart_item/<int:product_id>/<int:cart_item_id>/', views.remove_cart_item, name= 'remove_cart_item'),
+    path('delete_cart_item/<int:product_id>/<int:cart_item_id>/', views.delete_cart_item, name= 'delete_cart_item'),
+    
+]
